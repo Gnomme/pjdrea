dc54ab357215970e99fab66639aa79a8cd1f38f2
ishtarimu <ngc5427@gmail.com>
2013-07-04 17:19:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,16 @@
 <?php // php y�y� //
 	$parties = scandir("parties"); //scan les parties
 	$parties = array_slice($parties, 2); //on vire les 2 premiers
-	//chdir("parties"); // se fout dans le bon dir
-	//echo "$parties[2]"; //le nom du fichier brut //
 	
 	$partieNom = $partiePath = [];
 	
 	//LA boucle
 	for ($i = 0; $i < count($parties); $i++) {
-		$partieNom[$i] = "$parties[$i]";
+		$partieNom[$i] = "$parties[$i]"; //le nom du fichier brut //
 		$partieNom[$i] = str_replace("_"," ", $partieNom[$i]);
 		$partiePath[$i] = "parties/" . "$parties[$i]";
 		
-		//echo "$partieNom[$i]" . " " . "$partiePath[$i]" ;
+		//echo "$partieNom[$i]" . " " . "$partiePath[$i]"; // pour test
 	}
 ?>
 
@@ -37,10 +35,11 @@
 	</div>
 	<div id="PartiesEnCours">
 		<h1> Parties en cours </h1>
-		
+		<!-- ila boucle des partie "finie = 0" -->
 	</div>
 		<div id="PartiesFinies">
 		<h1> Parties Finies </h1>
+		<!-- la boucle des partie "finie = 1" -->
 	</div>
 </div>
 </body>
