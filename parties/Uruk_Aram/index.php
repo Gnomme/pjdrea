f77c57bec69b0d17a40d0870864bb4e1ff9a03ec
ishtarimu <ngc5427@gmail.com>
2013-07-04 18:50:37 +0200

    Update index.php

--- parties/Uruk_Aram/index.php
+++ parties/Uruk_Aram/index.php
@@ -1,1 +1,4 @@
-youp youp
+ici il faut afficher le nom et l'image de la partie
+le nom des joueurs.
+la liste des "texte" de la partie.
+un bouton pour entrer dans la version admin-joueur pour qu'ils puissent répondre/editer leur post.
